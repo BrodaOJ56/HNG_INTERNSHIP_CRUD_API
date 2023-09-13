124a07fb7145bd4c4edfab9b177174f07befa84b
BrodaOJ56 <olujas1@gmail.com>
2023-09-13 20:26:21 +0100

    updated

--- person_api/settings.py
+++ person_api/settings.py
@@ -131,6 +131,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
