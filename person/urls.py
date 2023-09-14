bbf9143768fddf5b41b7b5d210b1a6bd617b619d
BrodaOJ56 <olujas1@gmail.com>
2023-09-14 01:09:50 +0100

    updated

--- person/urls.py
+++ person/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.PersonList.as_view(), name='person-list'),
-    path('<int:pk>', views.PersonDetail.as_view(), name='person-detail'),
+    path('<pk>', views.PersonDetail.as_view(), name='person-detail'),
     path('', views.PersonCreate.as_view(), name='person-create'),
     path('<int:pk>', views.PersonUpdate.as_view(), name='person-update'),
     path('<int:pk>', views.PersonDelete.as_view(), name='person-delete'),
--- person/views.py
+++ person/views.py
@@ -1,12 +1,9 @@
 from rest_framework import generics
 from .models import Person
 from .serializers import PersonSerializer
+from rest_framework.exceptions import NotFound
 
 class PersonList(generics.ListCreateAPIView):
-    queryset = Person.objects.all()
-    serializer_class = PersonSerializer
-
-class PersonDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Person.objects.all()
     serializer_class = PersonSerializer
 
@@ -21,3 +18,21 @@ class PersonUpdate(generics.UpdateAPIView):
 class PersonDelete(generics.DestroyAPIView):
     queryset = Person.objects.all()
     serializer_class = PersonSerializer
+
+class PersonDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = PersonSerializer
+
+    def get_object(self):
+        lookup_field = 'id'  # Default to looking up by ID
+
+        # Check if the provided 'pk' is a number (ID) or not (name)
+        if not self.kwargs['pk'].isdigit():
+            lookup_field = 'name'
+
+        try:
+            obj = Person.objects.get(**{lookup_field: self.kwargs['pk']})
+        except Person.DoesNotExist:
+            raise NotFound("Person not found by ID or name.")
+
+        self.check_object_permissions(self.request, obj)
+        return obj
